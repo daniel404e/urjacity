1ec48d164f833d0e08fe6227f170d89f1a31b3d3
Daniel Joshua <joshuadaniel404@gmail.com>
2024-02-15 08:59:31 +0530

    dfgdsfgs

--- dbserver.js
+++ dbserver.js
@@ -149,6 +149,6 @@ app.post('/addnewannouncements', async (req, res) => {
  
   
 // Start the server
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
 });
